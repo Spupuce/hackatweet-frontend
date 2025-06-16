6a37d70600b96c45ebf554a09e467f05d08b4c6c
Spupuce <spupucemail@gmail.com>
2025-06-16 21:31:54 +0200

    fin 1er jour

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,45 @@
-import styles from '../styles/Home.module.css';
+import React, { useState, useEffect } from "react";
+
+import styles from "../styles/Home.module.css";
+
+//A enlever
+const name = "John";
+const email = "@johnCena";
 
 function Home() {
+  const [user, setUser] = useState(null);
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div className={styles.container}>
+      <aside className="sidebar-left">
+        <div className="high">
+          <img
+            src="../images/logo.png"
+            alt="Logo site"
+            className="logo"
+            width={85}
+          />
+        </div>
+        <div className="down">
+          <img src="../images/user.png" alt="Avatar" className="avatar"width={30}/>
+          <div className="user-info">
+            <strong>{name}</strong>
+            <p>{email}</p>
+            </div>
+          <button>Logout</button>
+        </div>
+      </aside>
+      <main>
+        <div class="input-box">
+          <h2>Home</h2>
+          <input type="text" placeholder="What's up ?"/>
+          <p>0/280</p>
+          <button>Tweet</button>
+        </div>
+        <div class="box">
+          <img src="../images/user.png" alt="Avatar" className="avatar"width={30}/>
+        </div>
       </main>
+      <aside></aside>
     </div>
   );
 }
